72232911b9818a43772bb047dd03bba4304a0194
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2021-04-23 17:10:57 +0000

    Automatically backed up by Learn

--- src/components/ThemedDecorations.js
+++ src/components/ThemedDecorations.js
@@ -1,7 +1,19 @@
 import React from 'react'
 
 class ThemedDecorations extends React.Component {
-  
+  render() {
+    const childWithClass = React.Children.map(this.props.children, child => {
+      return React.cloneElement(child, {
+        hasClass: child.props.class === 
+      });
+    });
+    
+    return(
+      <div className="">
+      
+      </div>
+    );
+  }
 }
 
 export default ThemedDecorations;
